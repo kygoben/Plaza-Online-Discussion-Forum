985baaedf528862b34105121e8a482595a83a4e2
Chris Tague <cstague6401@gmail.com>
2022-03-29 20:55:26 -0500

    adding post display

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Editor } from './editor';
 import { Button, buttonTypes } from './UIComponents/Button';
+import { PostDisplay } from './post-display';
 
 export const App = () => {
+    const [ text, setText ] = useState();
+
     return (
         <div className='app'>
-            <Editor/>
+            <Editor
+                text={text}
+                setText={setText}
+            />
             <Button
                 type={buttonTypes.Submit}
                 onClick={ () => {
                     window.alert('I am a button');
                 } }
             />
+            <PostDisplay html={text}/>
         </div>
     );
 }
--- src/editor/editor.jsx
+++ src/editor/editor.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
 import classNames from 'classnames';
+
 import styles from './editor.module.css';
 
-export const Editor = () => {
-    const [text, setText] = useState();
+export const Editor = ({
+    text,
+    setText
+}) => {
 
     return (
         <div className={classNames(styles.editor)}>
