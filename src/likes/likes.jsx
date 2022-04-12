ad8eca17f2cc2dc91c9a713d5e83126d5ef44be4
Kyle_Goben <kgoben@iastate.edu>
2022-04-12 15:27:43 -0500

    created like componets

---
+++ src/likes/likes.jsx
@@ -0,0 +1,28 @@
+import React , {useState} from "react";
+// import { useState } from "react/cjs/react.production.min";
+import styles from "./likes.module.css"
+
+export const Likes = (props) => {
+
+    const [likes, setLikes] = useState(props.votes);
+    return (
+        <div className={styles.wrapper}>
+            <button className={styles.upVote} onClick={() => setLikes(likes + 1)}>
+                ^
+            </button>
+
+            <span>
+                {likes}
+            </span>
+
+            <button className={styles.downVote} onClick={() => setLikes(likes - 1)}>
+                ^
+            </button>
+        </div>
+    )
+
+
+
+
+
+}
